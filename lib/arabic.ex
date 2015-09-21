56487e4442e76f22ba7e81b27dd00cb223f981f5
Rob Park <robert.d.park@gmail.com>
2015-09-21 08:30:51 -0400

    small refactor

--- lib/arabic.ex
+++ lib/arabic.ex
@@ -1,6 +1,7 @@
 defmodule Arabic do
   def to_roman(arabic), do: _to_roman(arabic, "")
 
+  defp _to_roman(arabic, roman) when arabic == 0, do: roman
   defp _to_roman(arabic, roman) when arabic == 1000, do: "#{roman}M"
   defp _to_roman(arabic, roman) when arabic == 500, do: "#{roman}D"
   defp _to_roman(arabic, roman) when arabic == 100, do: "#{roman}C"
@@ -9,7 +10,6 @@ defmodule Arabic do
   defp _to_roman(arabic, roman) when arabic == 9, do: "#{roman}IX"
   defp _to_roman(arabic, roman) when arabic == 5, do: "#{roman}V"
   defp _to_roman(arabic, roman) when arabic == 4, do: "#{roman}IV"
-  defp _to_roman(arabic, roman) when arabic == 1, do: "#{roman}I"
 
   defp _to_roman(arabic, roman) when arabic < 4 do
     _to_roman(arabic - 1, "#{roman}I")
