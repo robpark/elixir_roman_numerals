e1df1f0995c9664ef9a87a82e2fceb1dad6b5bc7
Rob Park <robert.d.park@gmail.com>
2015-09-28 22:57:12 -0400

    limit to 3 consecutive Xs

--- lib/arabic.ex
+++ lib/arabic.ex
@@ -1,6 +1,15 @@
 defmodule Arabic do
   def to_roman(arabic), do: _to_roman(arabic, "")
 
+  defp _to_roman(arabic, roman) when arabic < 0 do
+    chars = String.codepoints roman
+    position = String.length(roman) - 1
+    result_chars = List.insert_at chars, position, "I"
+    List.to_string result_chars
+  end
+
+  defp _to_roman(arabic, "XXXX"), do: _to_roman(arabic, "XL")
+
   defp _to_roman(arabic, roman) when arabic >= 999, do: _to_roman(arabic - 1000, "#{roman}M")
   defp _to_roman(arabic, roman) when arabic >= 499, do: _to_roman(arabic - 500, "#{roman}D")
   defp _to_roman(arabic, roman) when arabic >= 99, do: _to_roman(arabic - 100, "#{roman}C")
@@ -9,11 +18,4 @@ defmodule Arabic do
   defp _to_roman(arabic, roman) when arabic >= 4, do: _to_roman(arabic - 5, "#{roman}V")
   defp _to_roman(arabic, roman) when arabic >= 1, do: _to_roman(arabic - 1, "#{roman}I")
   defp _to_roman(arabic, roman) when arabic == 0, do: roman
-
-  defp _to_roman(arabic, roman) when arabic < 0 do
-    chars = String.codepoints roman
-    position = String.length(roman) - 1
-    result_chars = List.insert_at chars, position, "I"
-    List.to_string result_chars
-  end
 end
