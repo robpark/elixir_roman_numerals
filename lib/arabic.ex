e42ad6bcbfbf4a4fba5876a2e863c82fdf889019
Rob Park <robert.d.park@gmail.com>
2015-11-07 20:53:23 -0500

    convert additional values to roman; add tests to validate roman numeral rules

--- lib/arabic.ex
+++ lib/arabic.ex
@@ -1,10 +1,13 @@
 defmodule Arabic do
   def to_roman(arabic), do: _to_roman(arabic, "")
 
-  defp _to_roman(arabic, roman) when arabic < 0 do
+  defp _to_roman(arabic, roman) when arabic < -9, do: _to_roman_minus(arabic, roman, "X")
+  defp _to_roman(arabic, roman) when arabic < 0, do: _to_roman_minus(arabic, roman, "I")
+
+  defp _to_roman_minus(arabic, roman, digit) when arabic < 0 do
     chars = String.codepoints roman
     position = String.length(roman) - 1
-    result_chars = List.insert_at chars, position, "I"
+    result_chars = List.insert_at chars, position, digit
     List.to_string result_chars
   end
 
@@ -13,7 +16,7 @@ defmodule Arabic do
 
   defp _to_roman(arabic, roman) when arabic >= 999, do: _to_roman(arabic - 1000, "#{roman}M")
   defp _to_roman(arabic, roman) when arabic >= 499, do: _to_roman(arabic - 500, "#{roman}D")
-  defp _to_roman(arabic, roman) when arabic >= 99, do: _to_roman(arabic - 100, "#{roman}C")
+  defp _to_roman(arabic, roman) when arabic >= 90, do: _to_roman(arabic - 100, "#{roman}C")
   defp _to_roman(arabic, roman) when arabic >= 49, do: _to_roman(arabic - 50, "#{roman}L")
   defp _to_roman(arabic, roman) when arabic >= 9, do: _to_roman(arabic - 10, "#{roman}X")
   defp _to_roman(arabic, roman) when arabic >= 4, do: _to_roman(arabic - 5, "#{roman}V")
--- test/arabic_test.exs
+++ test/arabic_test.exs
@@ -8,17 +8,19 @@ defmodule ArabicTest do
      {:XXV, 25}, {:XXVI, 26}, {:XXVII, 27}, {:XXVIII, 28}, {:XXIX, 29}, {:XXX, 30}, {:XXXI, 31},
      {:XXXII, 32}, {:XXXIII, 33}, {:XXXIV, 34}, {:XXXV, 35}, {:XXXVI, 36}, {:XXXVII, 37},
      {:XXXVIII, 38}, {:XXXIX, 39}, {:XL, 40}, {:XLI, 41}, {:XLII, 42}, {:XLIII, 43}, {:XLIV, 44},
-     {:XLV, 45}, {:XLVI, 46}, {:XLVII, 47}, {:XLVIII, 48},
-     {:IL, 49}, {:L, 50}, {:LI, 51},
-     # {:XC, 90},
-     {:IC, 99}, {:C, 100}, {:CI, 101},
-     {:CD, 400}, {:ID, 499}, {:D, 500}, {:DI, 501},
-     {:IM, 999}, {:M, 1000}, {:MI, 1001}
+     {:XLV, 45}, {:XLVI, 46}, {:XLVII, 47}, {:XLVIII, 48}, {:IL, 49}, {:L, 50}, {:LI, 51},
+     {:LIV, 54}, {:LV, 55}, {:LXVII, 67}, {:LXIX, 69}, {:LXXXIX, 89}, {:XC, 90}, {:IC, 99},
+     {:C, 100}, {:CI, 101}, {:CD, 400}, {:ID, 499}, {:D, 500}, {:DI, 501}, {:IM, 999}, {:M, 1000},
+     {:MI, 1001}
    ]
 
   Enum.each @tuples, fn(pair) ->
-    test "From #{elem pair, 1}" do
+    test "from #{elem pair, 1}" do
       _assert_conversion(unquote(pair))
+    end
+
+    test "at most 3: #{elem pair, 1}" do
+      _assert_no_more_than_3(unquote(pair))
     end
   end
 
@@ -26,5 +28,13 @@ defmodule ArabicTest do
     arabic = elem tuple, 1
     roman = to_string elem(tuple, 0)
     assert Arabic.to_roman(arabic) == roman
+  end
+
+  defp _assert_no_more_than_3(tuple) do
+    roman = to_string elem(tuple, 0)
+    refute String.contains?(roman, "IIII"), "#{roman}: too many I"
+    refute String.contains?(roman, "XXXX"), "#{roman}: too many X"
+    refute String.contains?(roman, "CCCC"), "#{roman}: too many C"
+    refute String.contains?(roman, "MMMM"), "#{roman}: too many M"
   end
 end
