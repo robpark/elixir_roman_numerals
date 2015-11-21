ac73e7dfbd7dec40dd7a8309d799693883dfb19c
Rob Park <robert.d.park@gmail.com>
2015-11-21 21:34:18 +0100

    test from arabic and back 1..2000

--- test/combined_test.exs
+++ test/combined_test.exs
@@ -1,7 +1,7 @@
 defmodule CombinedTest do
   use ExUnit.Case
 
-  Enum.each 1..5, fn(arabic) ->
+  Enum.each 1..2000, fn(arabic) ->
     roman = Arabic.to_roman arabic
     computed_arabic = Roman.to_arabic roman
     test "#{arabic} -- #{roman} -- #{computed_arabic}" do
