e6350950d1fe8f0803f78b3deb845fb9953460a4
Rob Park <robert.d.park@gmail.com>
2015-11-21 21:03:44 +0100

    refactoring down the cases

--- lib/roman.ex
+++ lib/roman.ex
@@ -1,14 +1,20 @@
 defmodule Roman do
-  def to_arabic(roman), do: _to_arabic(String.to_char_list(roman), 0)
+  def to_arabic(roman), do: _to_arabic(String.to_char_list(roman), 0, '')
 
-  # defp _to_arabic([head|tail], arabic), do: head
+  defp _to_arabic([?C, ?M|tail], arabic, _), do: _to_arabic(tail, arabic + _basic_arabic(?M) - _basic_arabic(?C), ?M)
+  defp _to_arabic([?C, ?D|tail], arabic, _), do: _to_arabic(tail, arabic + _basic_arabic(?D) - _basic_arabic(?C), ?D)
+  defp _to_arabic([?X, ?C|tail], arabic, _), do: _to_arabic(tail, arabic + _basic_arabic(?C) - _basic_arabic(?X), ?C)
+  defp _to_arabic([?X, ?L|tail], arabic, _), do: _to_arabic(tail, arabic + _basic_arabic(?L) - _basic_arabic(?X), ?L)
+  defp _to_arabic([?I, ?X|tail], arabic, _), do: _to_arabic(tail, arabic + _basic_arabic(?X) - _basic_arabic(?I), ?X)
+  defp _to_arabic([?I, ?V|tail], arabic, _), do: _to_arabic(tail, arabic + _basic_arabic(?V) - _basic_arabic(?I), ?V)
+  defp _to_arabic([head|tail], arabic, _), do: _to_arabic(tail, arabic + _basic_arabic(head), head)
+  defp _to_arabic([], arabic, _), do: arabic
 
-  defp _to_arabic([head|tail], arabic) when head == ?M, do: _to_arabic(tail, arabic + 1000)
-  defp _to_arabic([head|tail], arabic) when head == ?D, do: _to_arabic(tail, arabic + 500)
-  defp _to_arabic([head|tail], arabic) when head == ?C, do: _to_arabic(tail, arabic + 100)
-  defp _to_arabic([head|tail], arabic) when head == ?L, do: _to_arabic(tail, arabic + 50)
-  defp _to_arabic([head|tail], arabic) when head == ?X, do: _to_arabic(tail, arabic + 10)
-  defp _to_arabic([head|tail], arabic) when head == ?V, do: _to_arabic(tail, arabic + 5)
-  defp _to_arabic([head|tail], arabic) when head == ?I, do: _to_arabic(tail, arabic + 1)
-  defp _to_arabic([], arabic), do: arabic
+  defp _basic_arabic(?M), do: 1000
+  defp _basic_arabic(?D), do: 500
+  defp _basic_arabic(?C), do: 100
+  defp _basic_arabic(?L), do: 50
+  defp _basic_arabic(?X), do: 10
+  defp _basic_arabic(?V), do: 5
+  defp _basic_arabic(?I), do: 1
 end
--- test/roman_test.exs
+++ test/roman_test.exs
@@ -2,29 +2,18 @@ defmodule RomanTest do
   use ExUnit.Case
 
   @tuples [
-     {:I, 1}, {:II, 2}, {:III, 3},
-     # {:IV, 4},
-     {:V, 5},
-     # {:VI, 6}, {:VII, 7}, {:VIII, 8}, {:IX, 9},
-     {:X, 10},
-     # {:XI, 11}, {:XII, 12}, {:XIII, 13}, {:XIV, 14}, {:XV, 15}, {:XVI, 16}, {:XVII, 17},
-     # {:XVIII, 18}, {:XIX, 19}, {:XX, 20}, {:XXI, 21}, {:XXII, 22}, {:XXIII, 23}, {:XXIV, 24},
-     # {:XXV, 25}, {:XXVI, 26}, {:XXVII, 27}, {:XXVIII, 28}, {:XXIX, 29}, {:XXX, 30}, {:XXXI, 31},
-     # {:XXXII, 32}, {:XXXIII, 33}, {:XXXIV, 34}, {:XXXV, 35}, {:XXXVI, 36}, {:XXXVII, 37},
-     # {:XXXVIII, 38}, {:XXXIX, 39}, {:XL, 40}, {:XLI, 41}, {:XLII, 42}, {:XLIII, 43}, {:XLIV, 44},
-     # {:XLV, 45}, {:XLVI, 46}, {:XLVII, 47}, {:XLVIII, 48}, {:XLIX, 49},
-     {:L, 50},
-     # {:LI, 51},
-     # {:LIV, 54}, {:LV, 55}, {:LXVII, 67}, {:LXIX, 69}, {:LXXXIX, 89}, {:XC, 90}, {:XCIX, 99},
-     {:C, 100},
-     # {:CI, 101}, {:CD, 400}, {:CDXCIX, 499},
-     {:D, 500},
-     # {:DI, 501}, {:DXI, 511},
-     # {:DCXLIX, 649}, {:DCCLXXVII, 777}, {:DCCC, 800}, {:DCCCIX, 809}, {:DCCCXCIX, 899},
-     # {:CMXLIX, 949}, {:CML, 950}, {:CMLI, 951}, {:CMLXXXIX, 989}, {:CMXC, 990}, {:CMXCI, 991},
-     # {:CMXCIX, 999},
-     {:M, 1000},
-     # {:MI, 1001}
+     {:I, 1}, {:II, 2}, {:III, 3}, {:IV, 4}, {:V, 5}, {:VI, 6}, {:VII, 7}, {:VIII, 8}, {:IX, 9},
+     {:X, 10}, {:XI, 11}, {:XII, 12}, {:XIII, 13}, {:XIV, 14}, {:XV, 15}, {:XVI, 16}, {:XVII, 17},
+     {:XVIII, 18}, {:XIX, 19}, {:XX, 20}, {:XXI, 21}, {:XXII, 22}, {:XXIII, 23}, {:XXIV, 24},
+     {:XXV, 25}, {:XXVI, 26}, {:XXVII, 27}, {:XXVIII, 28}, {:XXIX, 29}, {:XXX, 30}, {:XXXI, 31},
+     {:XXXII, 32}, {:XXXIII, 33}, {:XXXIV, 34}, {:XXXV, 35}, {:XXXVI, 36}, {:XXXVII, 37},
+     {:XXXVIII, 38}, {:XXXIX, 39}, {:XL, 40}, {:XLI, 41}, {:XLII, 42}, {:XLIII, 43}, {:XLIV, 44},
+     {:XLV, 45}, {:XLVI, 46}, {:XLVII, 47}, {:XLVIII, 48}, {:XLIX, 49}, {:L, 50}, {:LI, 51},
+     {:LIV, 54}, {:LV, 55}, {:LXVII, 67}, {:LXIX, 69}, {:LXXXIX, 89}, {:XC, 90}, {:XCIX, 99},
+     {:C, 100}, {:CI, 101}, {:CD, 400}, {:CDXCIX, 499}, {:D, 500}, {:DI, 501}, {:DXI, 511},
+     {:DCXLIX, 649}, {:DCCLXXVII, 777}, {:DCCC, 800}, {:DCCCIX, 809}, {:DCCCXCIX, 899},
+     {:CMXLIX, 949}, {:CML, 950}, {:CMLI, 951}, {:CMLXXXIX, 989}, {:CMXC, 990}, {:CMXCI, 991},
+     {:CMXCIX, 999}, {:M, 1000}, {:MI, 1001}
    ]
 
   Enum.each @tuples, fn(pair) ->
