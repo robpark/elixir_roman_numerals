a0d5d9f2ac915f727f68a628f0a478d3063a762a
Rob Park <robert.d.park@gmail.com>
2015-11-21 21:05:14 +0100

    remove parameter no longer needed

--- lib/roman.ex
+++ lib/roman.ex
@@ -1,14 +1,14 @@
 defmodule Roman do
-  def to_arabic(roman), do: _to_arabic(String.to_char_list(roman), 0, '')
+  def to_arabic(roman), do: _to_arabic(String.to_char_list(roman), 0)
 
-  defp _to_arabic([?C, ?M|tail], arabic, _), do: _to_arabic(tail, arabic + _basic_arabic(?M) - _basic_arabic(?C), ?M)
-  defp _to_arabic([?C, ?D|tail], arabic, _), do: _to_arabic(tail, arabic + _basic_arabic(?D) - _basic_arabic(?C), ?D)
-  defp _to_arabic([?X, ?C|tail], arabic, _), do: _to_arabic(tail, arabic + _basic_arabic(?C) - _basic_arabic(?X), ?C)
-  defp _to_arabic([?X, ?L|tail], arabic, _), do: _to_arabic(tail, arabic + _basic_arabic(?L) - _basic_arabic(?X), ?L)
-  defp _to_arabic([?I, ?X|tail], arabic, _), do: _to_arabic(tail, arabic + _basic_arabic(?X) - _basic_arabic(?I), ?X)
-  defp _to_arabic([?I, ?V|tail], arabic, _), do: _to_arabic(tail, arabic + _basic_arabic(?V) - _basic_arabic(?I), ?V)
-  defp _to_arabic([head|tail], arabic, _), do: _to_arabic(tail, arabic + _basic_arabic(head), head)
-  defp _to_arabic([], arabic, _), do: arabic
+  defp _to_arabic([?C, ?M|tail], arabic), do: _to_arabic(tail, arabic + _basic_arabic(?M) - _basic_arabic(?C))
+  defp _to_arabic([?C, ?D|tail], arabic), do: _to_arabic(tail, arabic + _basic_arabic(?D) - _basic_arabic(?C))
+  defp _to_arabic([?X, ?C|tail], arabic), do: _to_arabic(tail, arabic + _basic_arabic(?C) - _basic_arabic(?X))
+  defp _to_arabic([?X, ?L|tail], arabic), do: _to_arabic(tail, arabic + _basic_arabic(?L) - _basic_arabic(?X))
+  defp _to_arabic([?I, ?X|tail], arabic), do: _to_arabic(tail, arabic + _basic_arabic(?X) - _basic_arabic(?I))
+  defp _to_arabic([?I, ?V|tail], arabic), do: _to_arabic(tail, arabic + _basic_arabic(?V) - _basic_arabic(?I))
+  defp _to_arabic([head|tail], arabic), do: _to_arabic(tail, arabic + _basic_arabic(head))
+  defp _to_arabic([], arabic), do: arabic
 
   defp _basic_arabic(?M), do: 1000
   defp _basic_arabic(?D), do: 500
