893c02dd1757e38426de0e6e3af1f0207e9fa983
Rob Park <robert.d.park@gmail.com>
2015-11-21 21:11:56 +0100

    reduce subtraction repetition

--- lib/roman.ex
+++ lib/roman.ex
@@ -1,14 +1,16 @@
 defmodule Roman do
   def to_arabic(roman), do: _to_arabic(String.to_char_list(roman), 0)
 
-  defp _to_arabic([?C, ?M|tail], arabic), do: _to_arabic(tail, arabic + _basic_arabic(?M) - _basic_arabic(?C))
-  defp _to_arabic([?C, ?D|tail], arabic), do: _to_arabic(tail, arabic + _basic_arabic(?D) - _basic_arabic(?C))
-  defp _to_arabic([?X, ?C|tail], arabic), do: _to_arabic(tail, arabic + _basic_arabic(?C) - _basic_arabic(?X))
-  defp _to_arabic([?X, ?L|tail], arabic), do: _to_arabic(tail, arabic + _basic_arabic(?L) - _basic_arabic(?X))
-  defp _to_arabic([?I, ?X|tail], arabic), do: _to_arabic(tail, arabic + _basic_arabic(?X) - _basic_arabic(?I))
-  defp _to_arabic([?I, ?V|tail], arabic), do: _to_arabic(tail, arabic + _basic_arabic(?V) - _basic_arabic(?I))
+  defp _to_arabic([?C, ?M|tail], arabic), do: _to_arabic(tail, arabic + _subtract(?M, ?C))
+  defp _to_arabic([?C, ?D|tail], arabic), do: _to_arabic(tail, arabic + _subtract(?D, ?C))
+  defp _to_arabic([?X, ?C|tail], arabic), do: _to_arabic(tail, arabic + _subtract(?C, ?X))
+  defp _to_arabic([?X, ?L|tail], arabic), do: _to_arabic(tail, arabic + _subtract(?L, ?X))
+  defp _to_arabic([?I, ?X|tail], arabic), do: _to_arabic(tail, arabic + _subtract(?X, ?I))
+  defp _to_arabic([?I, ?V|tail], arabic), do: _to_arabic(tail, arabic + _subtract(?V, ?I))
   defp _to_arabic([head|tail], arabic), do: _to_arabic(tail, arabic + _basic_arabic(head))
   defp _to_arabic([], arabic), do: arabic
+
+  defp _subtract(from, minus), do: _basic_arabic(from) - _basic_arabic(minus)
 
   defp _basic_arabic(?M), do: 1000
   defp _basic_arabic(?D), do: 500
